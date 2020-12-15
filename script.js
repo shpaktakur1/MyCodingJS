8b0d8a0d92d8b66db140c94621eebbde28f57ff1
shpaktakur1 <74150435+shpaktakur1@users.noreply.github.com>
2020-12-15 18:31:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,46 @@
+
 'use strict';
 
-// const box = document.getElementById('box'); // данный метод может быть только один на странице. Получаем элемент
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+    
+};
 
-// console.log(box); // существует только внутри браузера
-
-// const btns = document.getElementsByTagName('button')[1]; // получили псвевдомассив. [1] - получаем только один элемент
-
-// console.log(btns);
-
-// const circles = document.getElementsByClassName('circle'); // метод ищет элементы через класс
-// console.log(circles);
-
-// const hearts = document.querySelectorAll('.heart'); // метод, который во внутри помещаем css-селектор
-
-// hearts.forEach(item => {
-//     console.log(item);     // item - каждый элемент находящийся в псевдомассиве
-// });
-
-// const oneHeart = document.querySelector('.heart'); // метод позволяет получить только один элемент со страницы
-// console.log(oneHeart);
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movelist = document.querySelector('.promo__interactive-list');
 
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart'),
-      wrapper = document.querySelector('.wrapper');
-
-// box.style.backgroundColor = 'blue'; 
-// box.style.width = '500px';
-
-box.style.cssText = 'background-color: blue; width: 500px';
-
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-// for (let i = 0; i < hearts.length; i++){
-//     hearts[i].style.backgroundColor = 'blue';
+// var ReplaceBackground = function(){
+//     document.body.style.background = 'url(C://Users//1234567//Desktop//bg.jpg)';
 // }
 
-hearts.forEach(item =>{
-    item.style.backgroundColor = 'blue';
+// document.addEventListener("DOMContentLoaded", ReplaceBackground);
+
+adv.forEach(item =>{
+    item.remove();
 });
 
+genre.textContent = 'Драма';
 
-const div = document.createElement('div'); // элемент существует только в JS
-// const text = document.createTextNode('Тут был я') // помещаем текст в nodы 
+poster.style.backgroundImage = 'url("C://Users//PRO100VANO//Desktop//bg.jpg")';
 
-div.classList.add('black'); // создан класс
- wrapper.append(div);            
+movelist.innerHTML = "";
 
-// wrapper.prepend(div);            Эти элементы позволяют легко управлять страницей
+movieDB.movies.sort();
 
-// hearts[0].before(div);
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
 
-// hearts[0].after(div);
-
-// circles[0].remove();
-
-// hearts[0].replaceWith(circles[0]);
-
-div.innerHTML = "<h1>Hello World!</h1>";
-
-// div.textContent= "Hello";
-
-div.insertAdjacentHTML("afterbegin", '<h2>Hello</h2'); //beforeend, afterend
